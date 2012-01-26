70ecf64ca6a0758ff11dced84a2f7064693dd186
Anthony Stevens <anthonys@crowdify.com>
2012-01-26 12:04:58 -0800

    Added VS 2010 examples

---
+++ VS 2010 Examples/WindowsAzureProject1/MvcWebRole1/Controllers/StudentsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWebRole1.Controllers
+{
+    public class StudentsController : Controller
+    {
+        //
+        // GET: /Students/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Students/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View(id);
+        }
+
+        //
+        // GET: /Students/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Students/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Students/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Students/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Students/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Students/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
