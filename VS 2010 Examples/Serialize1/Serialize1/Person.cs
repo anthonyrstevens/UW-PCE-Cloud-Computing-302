b288cf77ed0e59af4347ca93fe2a147044460c76
Anthony Stevens <anthonys@crowdify.com>
2012-01-26 12:10:02 -0800

    Added missing files from the Serialize1 solution

---
+++ VS 2010 Examples/Serialize1/Serialize1/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Serialize1
+{
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public string FirstName { get; set; }
+    }
+}
---
+++ VS 2010 Examples/Serialize1/Serialize1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Serialize1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person
+                        {
+                            LastName = "Harris",
+                            FirstName = "Scott"
+                        };
+
+            Console.Out.WriteLine("About to serialize the object...");
+            Console.Out.WriteLine("");
+            
+            var s = new XmlSerializer(typeof(Person));
+            s.Serialize(Console.Out, p);
+            
+            
+            // deserialize 
+            // exercise for the user
+
+            Console.Out.WriteLine();
+
+            Console.Out.WriteLine("Press Enter, yo!");
+            Console.In.ReadLine();
+        }
+    }
+}
